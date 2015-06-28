d87a425a0b4dc82ebf139a3d0b29db34cc02ef3e
Chris Bateman <chrisb808@gmail.com>
2015-06-28 02:59:36 +0000

    minified

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         ]
     },
     plugins: [
-        //new webpack.optimize.UglifyJsPlugin({minimize: true})
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
     ],
     resolve: {
         extensions: ['', '.js', '.jsx']

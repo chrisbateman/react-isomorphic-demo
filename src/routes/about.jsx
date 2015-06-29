36c571d97f07809858ea813a04a1fca104be6afb
Chris Bateman <chrisb808@gmail.com>
2015-06-29 13:33:35 +0000

    minor - spacing

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 export default class extends React.Component {
     render() {
         return (
-        	<div>
-        		<h2>About Page</h2>
-        		<p>dolor sit amet</p>
-        	</div>
+            <div>
+                <h2>About Page</h2>
+                <p>dolor sit amet</p>
+            </div>
         );
     }
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 export default class extends React.Component {
     render() {
         return (
-        	<div>
-        		<h2>Index Page</h2>
-        		<p>Lorem ipsum</p>
-        	</div>
+            <div>
+                <h2>Index Page</h2>
+                <p>Lorem ipsum</p>
+            </div>
         );
     }
 }
